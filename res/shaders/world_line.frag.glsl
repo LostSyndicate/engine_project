cc07edeb1e3ef1738b8822d9e9374cec8af06ce8
losts <lostsyndicate@yandex.com>
2023-04-26 23:20:39 -0500

    empty

--- res/shaders/world_line.frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-out vec4 frag;
-in vec3 pos;
-
-void main()
-{
-	frag = vec4(normalize(pos), 1.0);
-}
--- source/Graphics/Light.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by losts_n1cs3jj on 11/5/2022.
-//
-
-#include "Graphics/Light.h"
